74be2c879e8b160c7bd81ae1d6e171b93c895d26
JDJG <22802640+JDJGInc@users.noreply.github.com>
2024-06-17 23:29:17 -0400

    properly format the old code.
    ``isort . && black . -l120``

--- ClientConfig.py
+++ ClientConfig.py
@@ -1,23 +1,26 @@
-import discord
-import re
-from discord.ext import commands
 import os
+import re
 
-async def get_prefix(client,message):
-  extras = ["Soda*","So*","sb!","sb*"]
-  comp = re.compile("^(" + "|".join(map(re.escape, extras)) + ").*", flags=re.I)
-  match = comp.match(message.content)
-  if match is not None:
-    extras.append(match.group(1))
-  return commands.when_mentioned_or(*extras)(client, message)
+import discord
+from discord.ext import commands
 
-bot = commands.Bot(command_prefix=(get_prefix),intents = discord.Intents.all())
 
-bot.load_extension('jishaku')
+async def get_prefix(client, message):
+    extras = ["Soda*", "So*", "sb!", "sb*"]
+    comp = re.compile("^(" + "|".join(map(re.escape, extras)) + ").*", flags=re.I)
+    match = comp.match(message.content)
+    if match is not None:
+        extras.append(match.group(1))
+    return commands.when_mentioned_or(*extras)(client, message)
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-      try:
-        bot.load_extension(f'cogs.{filename[:-3]}')
-      except commands.errors.NoEntryPointError:
-        pass
+
+bot = commands.Bot(command_prefix=(get_prefix), intents=discord.Intents.all())
+
+bot.load_extension("jishaku")
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        try:
+            bot.load_extension(f"cogs.{filename[:-3]}")
+        except commands.errors.NoEntryPointError:
+            pass
--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-import discord, os , asyncio,traceback
-import ClientConfig, B
+import asyncio
+import os
+import traceback
 
-bot=ClientConfig.bot
+import B
+import discord
+
+import ClientConfig
+
+bot = ClientConfig.bot
+
 
 async def status_task():
-  await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name="Orders for Soda"))
-  await asyncio.sleep(40)
-  await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name="Bot PFP made by Dutchy#6127(thank you) :D"))
-  await asyncio.sleep(40)
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Activity(type=discord.ActivityType.listening, name="Orders for Soda"),
+    )
+    await asyncio.sleep(40)
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Activity(
+            type=discord.ActivityType.listening, name="Bot PFP made by Dutchy#6127(thank you) :D"
+        ),
+    )
+    await asyncio.sleep(40)
+
 
 async def startup():
-  await bot.wait_until_ready()
-  await status_task()
+    await bot.wait_until_ready()
+    await status_task()
+
 
 @bot.event
 async def on_ready():
-  print("Bot is Ready")
-  print(f"Logged in as {bot.user}")
-  print(f"Id: {bot.user.id}")
+    print("Bot is Ready")
+    print(f"Logged in as {bot.user}")
+    print(f"Id: {bot.user.id}")
 
 
 @bot.event
-async def on_error(event,*args,**kwargs):
-  more_information=os.sys.exc_info()
-  error_wanted=traceback.format_exc()
-  traceback.print_exc()
-  
-  #print(more_information[0])
+async def on_error(event, *args, **kwargs):
+    more_information = os.sys.exc_info()
+    error_wanted = traceback.format_exc()
+    traceback.print_exc()
+
+    # print(more_information[0])
+
 
 B.b()
 bot.loop.create_task(startup())
